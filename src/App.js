3a081f497273d01b0d24e37470e036f2786702d1
Axel <axel.man94gmail.com>
2023-05-08 11:53:45 -0600

    Agregando una navbar para simular una MPA

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
-import './assets/css/base/base.css';
+import './assets/css/base/base.css'
 import './assets/css/componentes/card.css'
 import Home from './pages/Home.jsx'
 import Sobre from './pages/Sobre'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Page404 from './pages/Page404'
+import Header from './components/Header'
 
 function App() {
 
   return (
     <>
       <Router>
+        <Header/>
         <Routes>
           <Route path='/' element={<Home />}/>
           <Route path='/sobre' element={<Sobre />} />
+          <Route path='*' element={<Page404 />} />
         </Routes>
       </Router>
     </>
